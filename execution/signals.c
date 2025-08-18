13ea0f4c93e1e32b9398e567e6281903881d5265
Mery-bou <bouchritmeryeme08@gmail.com>
2025-08-18 08:32:02 +0100

    finish

--- execution/signals.c
+++ execution/signals.c
@@ -6,7 +6,7 @@
 /*   By: mbouchri <mbouchri@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 10:05:04 by mbouchri          #+#    #+#             */
-/*   Updated: 2025/08/18 06:58:08 by mbouchri         ###   ########.fr       */
+/*   Updated: 2025/08/18 08:31:25 by mbouchri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ void sigint_prompt(int sig)
     write(1, "\n", 1);
     rl_replace_line("", 0);
     rl_on_new_line();
-    rl_redisplay();
     g_exit = 130;
 }
 
